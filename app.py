8833b3129b92e2d8051f9d2b6252f239bf776c72
AponteHector <hector.aponte8@upr.edu>
2021-11-08 21:12:44 -0400

    Co-authored-by: Leonardo Torres <leonardo.torres5@upr.edu> Co-authored-by: ROBERTO C HERNANDEZ MARTINEZ <roberto.hernandez13@upr.edu>

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(_name_)
+CORS(app)
+
+@app.route('/redpush/account', methods=['GET', 'POST'])
+def handleAccounts():  # put application's code here
+    return 'Hello World!'
+
+@app.route('/redpush/account/<int:account_id>', methods=['GET', 'PUT', 'DELETE'])
+def handleAccountById():  # put application's code here
+    return 'Hello World!'
+
+@app.route('/redpush/room', methods=['GET', 'POST'])
+def handleRooms():  # put application's code here
+    return 'Hello World!'
+
+@app.route('/redpush/room/<int:room_id>', methods=['GET', 'PUT', 'DELETE'])
+def handleRoomById():  # put application's code here
+    return 'Hello World!'
+
+@app.route('/redpush/event', methods=['GET', 'POST'])
+def handleEvents():  # put application's code here
+    return 'Hello World!'
+
+@app.route('/redpush/event/<int:event_id>', methods=['GET', 'PUT', 'DELETE'])
+def handleEventById():  # put application's code here
+    return 'Hello World!'
+
+@app.route('/redpush/event/invitees/<int:event_id>', methods=['GET', 'POST'])
+def handleInvitees():  # put application's code here
+    return 'Hello World!'
+
+@app.route('/redpush/event/invitees/<int:event_id>/<int:account_id>', methods=['GET', 'PUT', 'DELETE'])
+def handleInviteeById():  # put application's code here
+    return 'Hello World!'
+
+
+if _name_ == '_main_':
+    app.run()
+
---
+++ config/dbconfig.py
@@ -0,0 +1,7 @@
+pg_config = {
+    'user' : 'zdnudpfluqghgu',
+    'password' : 'ee7b013d3eeda6903d39a3f45f3ce40b42caa4d71ffcce3fb19ad1095a5c9c19',
+    'dbhost' : 'ec2-52-70-107-254.compute-1.amazonaws.com',
+    'dbname' : 'dbf63l08prptar',
+    'dbport' : 5432
+}
