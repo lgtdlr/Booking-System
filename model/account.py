d5cc5204c38a593f074efedc6edf46fdc22f7ba6
Leonardo Torres <leonardo.torres5@upr.edu>
2021-11-10 23:46:11 -0400

    Refactor account and room

--- model/account.py
+++ model/account.py
@@ -1,10 +1,12 @@
 from config.dbconfig import pg_config
 import psycopg2
 
+
 class AccountsDAO:
     def __init__(self):
-        connection_url = "dbname=%s user=%s password=%s port=%s host='%s'" %(pg_config['dbname'], pg_config['user'],
-                                                                  pg_config['password'], pg_config['dbport'], pg_config['dbhost'])
+        connection_url = "dbname=%s user=%s password=%s port=%s host='%s'" % (pg_config['dbname'], pg_config['user'],
+                                                                              pg_config['password'],
+                                                                              pg_config['dbport'], pg_config['dbhost'])
         self.conn = psycopg2.connect(connection_url)
 
     def getAllAccounts(self):
@@ -23,28 +25,25 @@ class AccountsDAO:
         result = cursor.fetchone()
         return result
 
-    def insertAccount(self, username, password ,full_name, role):
+    def insertAccount(self, username, password, full_name, role):
         cursor = self.conn.cursor()
         query = "INSERT INTO account (username, password ,full_name, role) VALUES (%s,%s,%s,%s) RETURNING account_id;"
-        cursor.execute(query, (username, password ,full_name, role,))
+        cursor.execute(query, (username, password, full_name, role,))
         account_id = cursor.fetchone()[0]
         self.conn.commit()
         return account_id
 
     def updateAccount(self, account_id, username, password, full_name, role):
         cursor = self.conn.cursor()
-        query= "UPDATE account SET username=%s, password=%s, full_name=%s, role=%s WHERE account_id=%s;"
-        cursor.execute(query, (username, password, full_name, role,account_id,))
+        query = "UPDATE account SET username = %s, password = %s, full_name = %s, role = %s WHERE account_id=%s;"
+        cursor.execute(query, (username, password, full_name, role, account_id,))
         self.conn.commit()
         return True
 
     def deleteAccount(self, account_id):
         cursor = self.conn.cursor()
         query = "DELETE FROM account WHERE account_id=%s;"
-        cursor.execute(query,(account_id,))
-        # determine affected rows
+        cursor.execute(query, (account_id,))
         affected_rows = cursor.rowcount
         self.conn.commit()
-        # if affected rows == 0, the account was not found and hence not deleted
-        # otherwise, it was deleted, so check if affected_rows != 0
-        return affected_rows !=0
+        return affected_rows != 0
--- model/room.py
+++ model/room.py
@@ -1,61 +1,60 @@
 from config.dbconfig import pg_config
 import psycopg2
 
+
 class RoomDAO:
     def __init__(self):
-        connection_url = "dbname=%s user=%s password=%s port=%s host='%s'" %(pg_config['dbname'], pg_config['user'],
-                          pg_config['password'], pg_config['dbport'], pg_config['dbhost'])
+        connection_url = "dbname=%s user=%s password=%s port=%s host='%s'" % (pg_config['dbname'], pg_config['user'],
+                                                                              pg_config['password'],
+                                                                              pg_config['dbport'], pg_config['dbhost'])
         print("connection url: ", connection_url)
         self.conn = psycopg2.connect(connection_url)
-        
 
-    #CRUD OPERATIONS
+    # CRUD OPERATIONS
 
     def getAllRooms(self):
         cursor = self.conn.cursor()
-        query =  "select * from room;"
+        query = "select * from room;"
         cursor.execute(query)
         result = []
         for row in cursor:
             result.append(row)
         return result
-    
-    def getRoomById(self,rid):
+
+    def getRoomById(self, room_id):
         cursor = self.conn.cursor()
-        query = "select * from room where rid = %s;"
-        cursor.execute(query, (rid,))
+        query = "select * from room where room_id = %s;"
+        cursor.execute(query, (room_id,))
         result = cursor.fetchone()
         return result
 
-    def insertRoom(self, rname, rcapacity,rtype):
+    def insertRoom(self, name, capacity, type):
         cursor = self.conn.cursor()
-        query = "insert into room (rname, rcapacity, rtype) values (%s,%s,%s) returning rid;"
+        query = "insert into room (name, capacity, type) values (%s,%s,%s) returning room_id;"
+        cursor.execute(query, (name, capacity, type))
         rid = cursor.fetchone()[0]
         self.conn.commit()
         return rid
 
-    def updateRoom(self, rid, rname, rcapacity,rtype):
+    def updateRoom(self, room_id, name, capacity, type):
         cursor = self.conn.cursor()
-        query = "update parts set rname=%s, rcapcity=%s, rtype=%s;"
-        cursor.execute(query,(rname,rcapacity,rtype,rid))
+        query = "update room set name=%s, capcity=%s, type=%s where room_id=%s;"
+        cursor.execute(query, (name, capacity, type, room_id))
         self.conn.commit()
         return True
 
-    def deleteRoom(self,rid):
+    def deleteRoom(self, room_id):
         cursor = self.conn.cursor()
-        query = "delete from room where rid=%s;"
-        cursor.execute(query,(rid,))
+        query = "delete from room where room_id=%s;"
+        cursor.execute(query, (room_id,))
         affected_rows = cursor.rowcount
         self.conn.commit()
         return affected_rows != 0
 
     # CRUD OPERATIONS FINISH
 
-    #def findAvailableRoom(self,startTime,endTime):
-       # cursor = self.conn.cursor()
-       # query = ""
-       # cursor.execute(query,(startTime,endTime))
-       # result = cursor.fetchall()
-    
-
-    
+    # def findAvailableRoom(self,startTime,endTime):
+    # cursor = self.conn.cursor()
+    # query = ""
+    # cursor.execute(query,(startTime,endTime))
+    # result = cursor.fetchall()
