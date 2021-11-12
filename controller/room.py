3fcaa4fe0e21cea24c2311de03819ff20512c2cb
AponteHector <hector.aponte8@upr.edu>
2021-11-11 22:08:54 -0400

    added room in controller plus  working on operations

---
+++ controller/room.py
@@ -0,0 +1,72 @@
+from flask import jsonify
+from model.room import RoomsDAO
+
+class BaseRoom:
+    def getAllPartsOld(self):
+        D1 = {"pid" : 1, "pname": "Tuerka", "pmaterial" : "steel", "pcolor" : "grey", "pprice" : 2.5}
+        D2 = {"pid" : 2, "pname": "Motor", "pmaterial" : "aluminum", "pcolor" : "black", "pprice" : 5000}
+        D3 = {"pid" : 3, "pname": "Panel", "pmaterial" : "madera", "pcolor" : "beige", "pprice" : 10}
+        result = [D1, D2, D3]
+        return jsonify(result)
+        #return "All Parts Method"
+
+    def build_map_dict(self, row):
+        result = {'room_id': row[0], 'name': row[1], 'capacity': row[2], 'type': row[3]}
+        return result
+
+    def build_attr_dict(self, room_id,name, capacity, type):
+        result = {'room_id': room_id, 'name': name, 'capacity': capacity, 'type': type}
+        return result
+
+    def getAllRooms(self):
+        dao = RoomsDAO()
+        room_list = dao.getAllRooms()
+        result_list = []
+        for row in room_list:
+            obj = self.build_map_dict(row)
+            result_list.append(obj)
+        return jsonify(result_list)
+
+    def getRoomById(self, pid):
+        dao = RoomsDAO()
+        room_tuple = dao.getRoomById(pid)
+        if not room_tuple:
+            return jsonify("Not Found"), 404
+        else:
+            result = self.build_map_dict(room_tuple)
+            return jsonify(result), 200
+
+    def addNewRoom(self, json):
+        rname = json['rname']
+        rcapacity = json['rcapacity']
+        rtype = json['rtype']
+        dao = RoomsDAO()
+        rid = dao.insertRoom(rname,rcapacity,rtype)
+        result = self.build_attr_dict(rid,rname,rcapacity,rtype)
+        return jsonify(result), 201
+
+    def updateRoom(self, json):
+        rname = json['rname']
+        rcapacity = json['rcapacity']
+        rtype = json['rtype']
+        rid = json['rid']
+        dao = RoomsDAO()
+        updated_code = dao.updateRoom(rid,rname,rcapacity,rtype)
+        result = self.build_attr_dict(rid,rname,rcapacity,rtype)
+        return jsonify(result), 200
+
+    def deleteRoom(self, rid):
+        dao = RoomsDAO()
+        result = dao.deleteRoom(rid)
+        if result:
+            return jsonify("DELETED"), 200
+        else:
+            return jsonify("NOT FOUND"), 404
+
+    def findAvailableRoom(self,json):
+        date = json['date']
+        start_time = json['start_time']
+        end_time = json['end_time']
+        dao = RoomsDAO
+        result = dao.findAvailableRoom(date,start_time,end_time)
+        return jsonify(result), 200
--- model/room.py
+++ model/room.py
@@ -53,8 +53,41 @@ class RoomDAO:
 
     # CRUD OPERATIONS FINISH
 
-    # def findAvailableRoom(self,startTime,endTime):
-    # cursor = self.conn.cursor()
-    # query = ""
-    # cursor.execute(query,(startTime,endTime))
-    # result = cursor.fetchall()
+    # def findAvailableRoom(self,startTime,endTime,date):
+    #     cursor = self.conn.cursor()
+    #     query = """SELECT name FROM room
+    #                WHERE room.room_id NOT IN (
+    #                SELECT room.room_id FROM room
+    #                INNER JOIN is_room_unavailable iru on room.room_id = iru.room_id
+    #                INNER JOIN timeslot on iru.timeslot_id = timeslot.timeslot_id
+    #                WHERE date = %s and start_time = %s and end_time = %s
+    #                UNION SELECT room.room_id FROM room
+    #                INNER JOIN event e on room.room_id = e.room_id
+    #                INNER JOIN occupies o on e.event_id = o.event_id
+    #                INNER JOIN timeslot t on t.timeslot_id = o.timeslot_id
+    #                WHERE date = %s and start_time = %s and end_time = %s;
+    #             """
+    #     cursor.execute(query, (startTime,endTime,date,startTime,endTime,date))
+    #     result = cursor.fetchall()
+    #     return result
+
+    # def whoAppointedRoom(self,rname,date,startTime,endTime):
+    #     cursor = self.conn.cursor()
+    #     query = """SELECT full_name FROM account
+    #                JOIN event e ON account.account_id = e.creator_id
+    #                INNER JOIN  room r ON r.room_id = e.room_id
+    #                INNER JOIN occupies o ON e.event_id = o.event_id
+    #                INNER JOIN  timeslot t ON t.timeslot_id = o.timeslot_id
+    #                WHERE name = %s AND date = %s AND start_time = %s AND end_time = %s;
+    #             """
+    #     cursor.execute(query,(rname,date,startTime,endTime))
+    #     result = cursor.fetchone()
+    #     return result
+
+    # def getAllDaySchedule(self,rname,date):
+    #     cursor.conn.cursor()
+    #     query = """SELECT start_time,end_time from timeslot
+    #                INNER JOIN is_room_unavailable iru on timeslot.timeslot_id = iru.timeslot_id
+    #                 inner join room r on r.room_id = iru.room_id
+    #                 WHERE r.name = %s AND 
+    #             """
