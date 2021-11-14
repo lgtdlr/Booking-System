97dea6d5613be8130ef219972f0e462a2e8fd23d
lgtdlr <leonardo.torres5@upr.edu>
2021-11-14 03:07:57 -0400

    feature: add invitee model and controller

---
+++ controller/invitee.py
@@ -0,0 +1,71 @@
+from flask import jsonify
+
+from model.invitee import InviteeDAO
+
+
+class BaseInvitee:
+
+    def build_map_dict(self, row):
+        result = {'account_id': row[0], 'event_id': row[1]}
+        return result
+
+    def build_attr_dict(self, account_id, event_id):
+        result = {'account_id': account_id, 'event_id': event_id}
+        return result
+
+    def getAllInvitees(self):
+        dao = InviteeDAO()
+        invitee_list = dao.getAllInvitees()
+        result_list = []
+        for row in invitee_list:
+            obj = self.build_map_dict(row)
+            result_list.append(obj)
+        return jsonify(result_list), 200
+
+    def getAllInviteesByEvent(self, event_id):
+        dao = InviteeDAO()
+        invitee_list = dao.getAllInviteesByEvent(event_id)
+        result_list = []
+        for row in invitee_list:
+            obj = self.build_map_dict(row)
+            result_list.append(obj)
+        return jsonify(result_list), 200
+
+    def getInviteeByUniqueId(self, account_id, event_id):
+        dao = InviteeDAO()
+        invitee_tuple = dao.getInviteeByUniqueId(account_id, event_id)
+        if not invitee_tuple:
+            return jsonify("Not Found"), 404
+        else:
+            result = self.build_map_dict(invitee_tuple)
+            return jsonify(result), 200
+
+    def insertInvitee(self, json):
+        account_id = json['account_id']
+        event_id = json['event_id']
+        dao = InviteeDAO()
+        if not isinstance(account_id, int):
+            account_id = tuple(account_id)
+            if len(account_id) > 1:
+                result = dao.insertMultipleInvitees(account_id, event_id)
+                return jsonify("Inserted row(s): " + str(result))
+
+        result = dao.insertInvitee(account_id, event_id)
+
+        return jsonify("Inserted row(s): " + str(result))
+
+    def updateInvitee(self, account_id, event_id, json):
+        new_account_id = json['account_id']
+        new_event_id = json['event_id']
+        dao = InviteeDAO()
+        is_updated = dao.updateInvitee(account_id, event_id, new_account_id, new_event_id)
+        result = self.build_attr_dict(new_account_id, new_event_id)
+        return jsonify(is_updated)
+
+    def deleteInvitee(self, account_id, event_id):
+        dao = InviteeDAO()
+        result = dao.deleteInvitee(account_id, event_id)
+        if result:
+            return jsonify("DELETED"), 200
+        else:
+            return jsonify("NOT FOUND"), 404
---
+++ model/invitee.py
@@ -0,0 +1,93 @@
+from config.dbconfig import pg_config
+import psycopg2
+import psycopg2.extras
+
+
+class InviteeDAO:
+    def __init__(self):
+        connection_url = "dbname=%s user=%s password=%s port=%s host='%s'" % (pg_config['dbname'], pg_config['user'],
+                                                                              pg_config['password'],
+                                                                              pg_config['dbport'], pg_config['dbhost'])
+        self.conn = psycopg2.connect(connection_url)
+
+    def getAllInvitees(self):
+        cursor = self.conn.cursor()
+        query = "SELECT account_id, event_id FROM is_invited;"
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getAllInviteesByEvent(self, event_id):
+        cursor = self.conn.cursor()
+        query = "SELECT account_id, event_id from is_invited WHERE event_id = %s;"
+        cursor.execute(query, (event_id,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getInviteeByUniqueId(self, account_id, event_id):
+        cursor = self.conn.cursor()
+        query = "SELECT account_id, event_id from is_invited WHERE account_id = %s AND event_id = %s;"
+        cursor.execute(query, (account_id, event_id,))
+        result = cursor.fetchone()
+        return result
+
+    def insertInvitee(self, account_id, event_id):
+        cursor = self.conn.cursor()
+        query = "INSERT INTO is_invited (account_id, event_Id) VALUES (%s,%s) ON CONFLICT DO NOTHING RETURNING " \
+                "account_id, event_id;"
+        try:
+            cursor.execute(query, (account_id, event_id,))
+        except psycopg2.IntegrityError:
+            self.conn.rollback()
+            return None
+        else:
+            self.conn.commit()
+            affected_rows = cursor.rowcount
+            return affected_rows
+
+    def insertMultipleInvitees(self, account_ids, event_id):
+        values = []
+        for account in account_ids:
+            values.append([account, event_id])
+        cursor = self.conn.cursor()
+        try:
+            query = "INSERT INTO is_invited (account_id, event_id) VALUES %s ON CONFLICT DO NOTHING;"
+            psycopg2.extras.execute_values(
+                cursor, query, values, template=None, page_size=100
+            )
+        except psycopg2.IntegrityError:
+            self.conn.rollback()
+            return None
+        else:
+            self.conn.commit()
+            affected_rows = cursor.rowcount
+            return affected_rows
+
+    def updateInvitee(self, account_id, event_id, new_account_id, new_event_id):
+        cursor = self.conn.cursor()
+        query = "UPDATE is_invited SET account_id = %s, event_id = %s WHERE account_id= %s AND event_id = %s;"
+        try:
+            cursor.execute(query, (new_account_id, new_event_id, account_id, event_id,))
+        except psycopg2.IntegrityError:
+            self.conn.rollback()
+            return False
+        else:
+            self.conn.commit()
+            affected_rows = cursor.rowcount
+            return affected_rows != 0
+
+    def deleteInvitee(self, account_id, event_id):
+        cursor = self.conn.cursor()
+        query = "DELETE FROM is_invited WHERE account_id = %s AND event_id = %s;"
+        try:
+            cursor.execute(query, (account_id, event_id))
+        except psycopg2.IntegrityError:
+            self.conn.rollback()
+        else:
+            affected_rows = cursor.rowcount
+            self.conn.commit()
+            return affected_rows != 0
