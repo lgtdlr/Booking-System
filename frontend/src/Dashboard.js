552375fadcd3205bfe64c9dff3899b503d1c07ab
roberto <roberto.hernandez13@upr.edu>
2021-12-07 05:11:12 -0400

    added get most booked users

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -7,6 +7,7 @@ import {Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis} from "recha
 import axios from "axios";
 
 let mostBookingsWith = [];
+let mostBookedUsers = [];
 
 async function getMostBookingWithSelectedUser() {
     const token = sessionStorage.getItem("token");
@@ -20,18 +21,42 @@ async function getMostBookingWithSelectedUser() {
     })
 }
 
-function BookMeeting(){
 
+async function getMostBookedUsers() {
+
+    const url = "http://127.0.0.1:5000/redpush";
+
+    await axios.get(url+'/account/booked-users').then(function
+        (response) {
+        mostBookedUsers = response.data
+    })
+}
+
+
+function BookMeeting(){
+        //data represents users with whom registered has the most bookings with
     const [data, setData] = useState([{"name": 1, "Counts": 5},
                                                 {"name": 2, "Counts": 4},
                                                 {"name": 3, "Counts": 3},
                                                 {"name": 4, "Counts": 2},
                                                 {"name": 5, "Counts": 1}]);
+
+    const [mostBooked,setMostBooked] = useState([{"name": 1, "Counts": 1},
+                                                            {"name": 2, "Counts": 4}])
+
+
+
     useEffect(() => {
         getMostBookingWithSelectedUser().then(  () => {
             setData(mostBookingsWith)
             console.log("Bookings with" + mostBookingsWith)
         })
+
+        getMostBookedUsers().then( ()=>{
+            setMostBooked(mostBooked)
+            console.log(mostBookedUsers)
+            }
+        )
 
     }, [])
 
@@ -47,36 +72,17 @@ function BookMeeting(){
     //     </BarChart>
     // </Container>
 
-  // return <ul>
-  //       {data.map(item => (
-  //         <li key={item.account_id}>
-  //           <div>{item.name}</div>
-  //           <div>{item.Counts}</div>
-  //         </li>
-  //       ))}
-  //     </ul>
-
-// return (
-//
-//     <List horizontal ordered> {data.map (item => (<List.Item key={item.account_id}>
-//       <Image avatar src='https://react.semantic-ui.com/images/avatar/small/tom.jpg' />
-//       <List.Content>
-//         <List.Header>{item.name}</List.Header>
-//           {item.Counts}
-//       </List.Content>
-//     </List.Item>))}
-//   </List>)
-
     return (
         <div><Header as='h1'> You share a maximum of {(data.length !== 0 ? data[0].Counts : 0)} bookings with these users </Header>
-        <List horizontal ordered> {data.map (item=> (<List.Item key={item.account_id}>
-      <Image avatar src='https://react.semantic-ui.com/images/avatar/small/tom.jpg' />
-      <List.Content>
-        <List.Header>{item.name}</List.Header>
+        <List horizontal ordered> {data.map (item=>
+      (<List.Item key={item.account_id}>
+          <Image avatar src='https://react.semantic-ui.com/images/avatar/small/tom.jpg' />
+          <List.Content>
+             <List.Header>{item.name}</List.Header>
 
-      </List.Content>
-    </List.Item>))}
-  </List>
+          </List.Content>
+        </List.Item>))}
+      </List>
         </div>
 
     )
