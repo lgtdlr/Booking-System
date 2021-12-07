9661eaaeef9c5dea4d2b63f0a178d1d2063fded8
roberto <roberto.hernandez13@upr.edu>
2021-12-06 22:40:00 -0400

    added data

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -6,37 +6,44 @@ import {Button, Card, Container, Modal} from "semantic-ui-react";
 import {Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis} from "recharts";
 import axios from "axios";
 
-var mostBookingsWith = [];
+//let mostBookingsWith = [];
 
-async function getMostBookingWithSelectedUser() {
+// async function getMostBookingWithSelectedUser() {
+//     const token = sessionStorage.getItem("token");
+//     const url = "http://127.0.0.1:5000/redpush";
+//     const requestOptions = {
+//         headers: { Authorization: "Bearer " + token }
+//     };
+//     axios.get(url+'/account/bookings-with-user', requestOptions).then(function
+//         (response) {
+//         mostBookingsWith = response.data
+//             console.log(mostBookingsWith)
+//
+//
+//     })
+// }
+
+function BookMeeting(){
     const token = sessionStorage.getItem("token");
     const url = "http://127.0.0.1:5000/redpush";
     const requestOptions = {
         headers: { Authorization: "Bearer " + token }
     };
-    axios.get(url+'/account/bookings-with-user', requestOptions).then(function
-        (response) {
-        mostBookingsWith = response.data
-        for (let i = 0; i < mostBookingsWith.length; i++) {
-            mostBookingsWith.push(mostBookingsWith[i])
-        }
-    })
-}
+    let mostBookingsWith = [];
 
-function BookMeeting(){
-
-    const [data, setData] = useState([{"name": 1, "Counts": 5},
-                                                {"name": 2, "Counts": 4},
-                                                {"name": 3, "Counts": 3},
-                                                {"name": 4, "Counts": 2},
-                                                {"name": 5, "Counts": 1}]);
+      //const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
     useEffect(() => {
-        getMostBookingWithSelectedUser().then(r => {
+        axios.get(url+'/account/bookings-with-user', requestOptions)
+        .then(function
+                (response) {
+                mostBookingsWith = response.data
+                setData(mostBookingsWith)
 
-            setData(mostBookingsWith)
-            console.log("Bookings with" + mostBookingsWith)
-        })
 
+
+    })
+    console.log(data)
     }, [])
 
     return <Container style={{ height: 800 }}>
