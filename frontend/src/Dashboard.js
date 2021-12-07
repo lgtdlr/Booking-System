592e1a1b2f26c06b0dd5dd3ebce1a9c02d745474
roberto <roberto.hernandez13@upr.edu>
2021-12-07 03:49:26 -0400

    added most booked users with registered user

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -2,7 +2,7 @@ import React, {Component, useEffect, useState} from 'react';
 import {Calendar, momentLocalizer, Views } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment';
-import {Button, Card, Container, Modal} from "semantic-ui-react";
+import {Button, Card, Container, Header, Image, List, Modal} from "semantic-ui-react";
 import {Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis} from "recharts";
 import axios from "axios";
 
@@ -31,18 +31,51 @@ function BookMeeting(){
 
     }, [])
 
-    return <Container style={{ height: 800 }}>
+    // return <Container style={{ height: 800 }}>
+    //
+    //     <BarChart width={730} height={250} data={data}>
+    //         <CartesianGrid strokeDasharray="3 3" />
+    //         <XAxis dataKey="name" />
+    //         <YAxis />
+    //         <Tooltip />
+    //         <Legend />
+    //         <Bar dataKey="Counts" fill="#8884d8" />
+    //     </BarChart>
+    // </Container>
 
-        <BarChart width={730} height={250} data={data}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="Counts" fill="#8884d8" />
-        </BarChart>
-    </Container>
+  // return <ul>
+  //       {data.map(item => (
+  //         <li key={item.account_id}>
+  //           <div>{item.name}</div>
+  //           <div>{item.Counts}</div>
+  //         </li>
+  //       ))}
+  //     </ul>
 
+// return (
+//
+//     <List horizontal ordered> {data.map (item => (<List.Item key={item.account_id}>
+//       <Image avatar src='https://react.semantic-ui.com/images/avatar/small/tom.jpg' />
+//       <List.Content>
+//         <List.Header>{item.name}</List.Header>
+//           {item.Counts}
+//       </List.Content>
+//     </List.Item>))}
+//   </List>)
+
+    return (
+        <div><Header as='h1'> You share a maximum of {data[0].Counts} bookings with these users </Header>
+        <List horizontal ordered> {data.map (item=> (<List.Item key={item.account_id}>
+      <Image avatar src='https://react.semantic-ui.com/images/avatar/small/tom.jpg' />
+      <List.Content>
+        <List.Header>{item.name}</List.Header>
+
+      </List.Content>
+    </List.Item>))}
+  </List>
+        </div>
+
+    )
 
 }
 export default BookMeeting;
